71bfe11ed89f172e380c34c1ad38d7e02a028730
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2023-10-21 12:51:52 +0530

    add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const navList = document.querySelector(".nav-list");
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
+const navigation = document.querySelector(".navigation");
 
 menu.addEventListener("click", function () {
   navList.style.display = "inline-block";
@@ -13,3 +14,15 @@ close.addEventListener("click", function () {
   menu.style.display = "inline-block";
   close.style.display = "none";
 });
+
+
+
+
+window.onscroll = function() {
+  console.log(scrollY)
+  if(scrollY > 200){
+      navigation.classList.add("scroll");
+  }else{
+    navigation.classList.remove("scroll");
+  }
+};
